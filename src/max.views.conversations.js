5ede9866e680a870c152c93beb516834d54c2acb
Carles Bruguera <carlesba@gmail.com>
2014-05-08 17:30:06 +0200

    Fix animation bug

--- src/max.views.conversations.js
+++ src/max.views.conversations.js
@@ -113,6 +113,7 @@ var views = function() {
 
     MaxConversationsList.prototype.show = function() {
         var self = this;
+        self.mainview.loadWrappers();
         self.mainview.$newparticipants.show();
          // Load conversations from max if never loaded
         if (self.conversations.length === 0) {
