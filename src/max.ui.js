e9ac7161c50a3ceb821e1863aa36ee54f684cf55
Carles Bruguera <carlesba@gmail.com>
2014-06-30 11:52:42 +0200

    New 4.0.12 release

--- src/max.ui.js
+++ src/max.ui.js
@@ -10,7 +10,7 @@
     jq.fn.maxUI = function(options) {
         // Keep a reference of the context object
         var maxui = this;
-        maxui.version = '4.0.11';
+        maxui.version = '4.0.12';
         maxui.templates = max.templates();
         maxui.utils = max.utils();
         var defaults = {
