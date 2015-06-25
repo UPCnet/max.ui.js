14cec70bc7417cbaa12212bf39f4eafc90c9f0d0
Victor Fernandez de Alba <sneridagh@gmail.com>
2015-06-25 18:48:41 +0200

    Rebuild version

--- src/max.ui.js
+++ src/max.ui.js
@@ -8,7 +8,7 @@
     jq.fn.maxUI = function(options) {
         // Keep a reference of the context object
         var maxui = this;
-        maxui.version = '4.1.6';
+        maxui.version = '4.1.7';
         maxui.templates = max.templates();
         maxui.utils = max.utils();
         var defaults = {
