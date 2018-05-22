1ec61d4a684ad2eb01a1f8ce818d077675ca955f
Pilar Marinas <pilar.marinas@upcnet.es>
2018-05-22 15:56:08 +0200

    Release 4.1.24

--- src/max.ui.js
+++ src/max.ui.js
@@ -11,7 +11,7 @@
     jq.fn.maxUI = function(options) {
         // Keep a reference of the context object
         var maxui = this;
-        maxui.version = '4.1.23';
+        maxui.version = '4.1.24';
         maxui.templates = max.templates();
         maxui.utils = max.utils();
         var defaults = {
